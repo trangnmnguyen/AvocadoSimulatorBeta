2679c5e18d1590bb7961dea929b3b94cce6a39cb
Trang Nguyen <nguyeng@mail.uc.edu>
2019-04-23 09:15:11 -0400

    Made visibility of enum to public

--- AvocadoSimulator01/src/main/Main.java
+++ AvocadoSimulator01/src/main/Main.java
@@ -3,17 +3,25 @@
  * nicholdw@ucmail.uc.edu
  */
 package main;
+
+import avocado.Avocado;
+
 /***
  * Exercise our Avocado classes
  * @author nicomp
  *
  */
 public class Main {
-	enum myEnum {A,B,C,D};	// Demo of enumerated date type
+//	enum myEnum {A,B,C,D};	// Demo of enumerated date type
+//	enum enumCondition {fresh, ok, squishy, useless};
 
 	public static void main(String[] args) {
-		myEnum e = myEnum.A;
-		System.out.println(e.toString());
+		//Declare and instantiate an avocado object
+		Avocado bob = new Avocado("green", Avocado.enumCondition.fresh, 3);
+		
+	//		myEnum e = myEnum.A;
+	//		e = myEnum.B;
+	//		System.out.println(e.toString());
 
 	}
 }
